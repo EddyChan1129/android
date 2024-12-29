bae4584d1a03da193274a69f7d447b28270cd16e
EddyChan1129 <22026266d@connect.polyu.hk>
2024-12-29 17:38:04 -0500

    update function completed

--- app/src/main/java/com/example/notesqllite/NoteDatabaseHelper.kt
+++ app/src/main/java/com/example/notesqllite/NoteDatabaseHelper.kt
@@ -57,4 +57,31 @@ class NoteDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         db.close()
         return notesList
     }
+
+    fun updateNote (note: Note) {
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE, note.title)
+            put(COLUMN_CONTENT, note.content)
+        }
+        db.update(TABLE_NAME, values, "$COLUMN_ID = ?", arrayOf(note.id.toString()))
+        db.close()
+    }
+
+    fun getNoteByID(nodeId: Int): Note? {
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID = $nodeId"
+        val cursor = db.rawQuery(query, null)
+        cursor.moveToFirst()
+
+        val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+        val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+        val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+        cursor.close()
+        db.close()
+        return Note(
+            id, title, content
+        )
+    }
 }
